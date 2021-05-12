430c6778ac174437e783518c0ba04b9b2429398c
shaheemMPM <shaheem.mpm@gmail.com>
2021-05-12 12:31:01 +0530

    Adds articles

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const about = () => {
+  return ( 
+    <>
+      <Head>
+        <title>WebDev News | About</title>
+      </Head>
+      <h1>About Page</h1>
+    </>
+  );
+}
+ 
+export default about;
